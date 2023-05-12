913a50b17e97e03aa9374fd33650fba1e7af7809
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-05-12 02:53:14 +0200

    implem

--- implem/ei_application.c
+++ implem/ei_application.c
@@ -11,64 +11,37 @@
 
 
 
-/**
- * \brief	Creates an application.
- *		<ul>
- *			<li> initializes the hardware (calls \ref hw_init), </li>
- *			<li> registers all classes of widget, </li>
- *			<li> creates the root window (either in a system window, or the entire screen), </li>
- *			<li> creates the root widget to access the root window. </li>
- *		</ul>
- *
- * @param	main_window_size	If "fullscreen is false, the size of the root window of the
- *					application.
- *					If "fullscreen" is true, the current monitor resolution is
- *					used as the size of the root window. \ref hw_surface_get_size
- *					can be used with \ref ei_app_root_surface to get the size.
- * @param	fullScreen		If true, the root window is the entire screen. Otherwise, it
- *					is a system window.
- */
-void ei_app_create(ei_size_t main_window_size, bool fullscreen);
+void ei_app_create(ei_size_t main_window_size, bool fullscreen)
+{
+    /* A implémenter */
+}
 
-/**
- * \brief	Releases all the resources of the application, and releases the hardware
- *		(ie. calls \ref hw_quit).
- */
-void ei_app_free(void);
+void ei_app_free(void)
+{
+    /* A implémenter */
+}
 
-/**
- * \brief	Runs the application: enters the main event loop. Exits when
- *		\ref ei_app_quit_request is called.
- */
-void ei_app_run(void);
+void ei_app_run(void)
+{
+    /* A implémenter */
+}
 
-/**
- * \brief	Adds a rectangle to the list of rectangles that must be updated on screen. The real
- *		update on the screen will be done at the right moment in the main loop.
- *
- * @param	rect		The rectangle to add, expressed in the root window coordinates.
- *				A copy is made, so it is safe to release the rectangle on return.
- */
-void ei_app_invalidate_rect(const ei_rect_t* rect);
+void ei_app_invalidate_rect(const ei_rect_t* rect)
+{
+    /* A implémenter */
+}
 
-/**
- * \brief	Tells the application to quit. Is usually called by an event handler (for example
- *		when pressing the "Escape" key).
- */
-void ei_app_quit_request(void);
+void ei_app_quit_request(void)
+{
+    /* A implémenter */
+}
 
-/**
- * \brief	Returns the "root widget" of the application: a "frame" widget that span the entire
- *		root window.
- *
- * @return 			The root widget.
- */
-ei_widget_t ei_app_root_widget(void);
+ei_widget_t ei_app_root_widget(void)
+{
+    /* A implémenter */
+}
 
-/**
- * \brief	Returns the surface of the root window. Can be used to create surfaces with similar
- * 		r, g, b channels.
- *
- * @return 			The surface of the root window.
- */
-ei_surface_t ei_app_root_surface(void);
+ei_surface_t ei_app_root_surface(void)
+{
+    /* A implémenter */
+}
--- implem/ei_implementation.c
+++ implem/ei_implementation.c
@@ -12,18 +12,7 @@
 #include "ei_utils.h"
 
 
-/**
- * \brief	Converts the red, green, blue and alpha components of a color into a 32 bits integer
- * 		than can be written directly in the memory returned by \ref hw_surface_get_buffer.
- * 		The surface parameter provides the channel order.
- *
- * @param	surface		The surface where to store this pixel, provides the channels order.
- * @param	color		The color to convert.
- *
- * @return 			The 32 bit integer corresponding to the color. The alpha component
- *				of the color is ignored in the case of surfaces that don't have an
- *				alpha channel.
- */
+
 uint32_t ei_impl_map_rgba(ei_surface_t surface, ei_color_t color)
 {
 	int ir, ig, ib, ia;
@@ -45,31 +34,19 @@ uint32_t ei_impl_map_rgba(ei_surface_t surface, ei_color_t color)
 	return color_32;
 }
 
-/**
- * \brief	Tells the placer to recompute the geometry of a widget.
- *		The widget must have been previously placed by a call to \ref ei_place.
- *		Geometry re-computation is necessary for example when the text label of
- *		a widget has changed, and thus the widget "natural" size has changed.
- *
- * @param	widget		The widget which geometry must be re-computed.
- */
-void ei_impl_placer_run(ei_widget_t widget);
+void ei_impl_placer_run(ei_widget_t widget)
+{
+    /* A implémenter! */
+}
 
-/**
- * @brief	Draws the children of a widget.
- * 		The children are draw withing the limits of the clipper and
- * 		the widget's content_rect.
- *
- * @param	widget		The widget which children are drawn.
- * @param	surface		A locked surface where to draw the widget's children.
- * @param	pick_surface	The picking offscreen.
- * @param	clipper		If not NULL, the drawing is restricted within this rectangle
- *				(expressed in the surface reference frame).
- */
+
 void ei_impl_widget_draw_children      (ei_widget_t		widget,
 					ei_surface_t		surface,
 					ei_surface_t		pick_surface,
-					ei_rect_t*		clipper);
+					ei_rect_t*		clipper)
+{
+    /* A implémenter! */
+}
 
 void ei_fill_pixel(ei_surface_t surface, const ei_color_t *color, ei_point_t pixel)
 {
